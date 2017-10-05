02dba8ac37c0e48f17ae7e3a3e6bfb9cfa730100
Travis Surmi <surmi.travis@gmail.com>
2017-10-05 13:54:20 -0700

    changed footer image

--- client/src/components/structure/Footer.js
+++ client/src/components/structure/Footer.js
@@ -7,8 +7,10 @@ const styles = {
     bottom: 0,
     left: 0,
     right: 0,
-    height: 35,
-    backgroundColor: '#2980b9',
+    height: 50,
+    background: {
+      image: 'url("http://i68.tinypic.com/29c998l.png")'
+    },
     display: 'flex',
     alignItems: 'center',
     paddingLeft: 20
