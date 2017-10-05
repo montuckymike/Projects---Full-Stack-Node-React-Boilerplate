69fafd6a8317b9e9b6550f73cf98109b6131420d
Hannah Hendrickson <hannahhendrickson@Hannahs-MacBook-Pro.local>
2017-10-05 12:51:09 -0600

    Added navbar and styling

--- client/src/components/structure/header/Header.js
+++ client/src/components/structure/header/Header.js
@@ -1,11 +1,45 @@
 import React from 'react'
+import NavBar from './NavBar'
+import injectSheet from 'react-jss'
 
-const Header = () => {
+const styles = {
+  masterDiv: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  navPanel: {
+    width: '75%'
+  },
+  gravatar: {
+    width: '25%',
+    display: 'flex',
+    justifyContent: 'flex-end'
+  },
+  gImg: {
+    border: 'solid purple',
+    height: '2em',
+    width: '2em',
+    borderRadius: '25%'
+  }
+}
+const enhancer = injectSheet(styles)
+
+const Header = (props) => {
   return (
-    <div>
-    Header
+    <div className={props.classes.masterDiv}>
+      <div className={props.classes.navPanel}>
+        <NavBar />
+      </div>
+      <div className={props.classes.gravatar}>
+        <input type='text' placeholder='your name'></input>
+        <img className={props.classes.gImg} src='https://upload.wikimedia.org/wikipedia/commons/e/ea/Re_south_park_new_avatar.png' />
+      </div>
     </div>
   )
 }
+// Header.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// }
 
-export default Header
+export default enhancer(Header)
---
+++ client/src/components/structure/header/NavBar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+import injectSheet from 'react-jss'
+
+const styles = {
+  navLinks: {
+    border: 'solid red',
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  links: {
+    textDecoration: 'none',
+    color: 'blue'
+  }
+}
+const enhancer = injectSheet(styles)
+
+const NavBar = (props) => {
+  return (
+    <div className={props.classes.navLinks}>
+      <NavLink className={props.classes.links} to='/'>Home</NavLink>
+      <NavLink className={props.classes.links} to='/AddDrink'>AddDrink</NavLink>
+      <NavLink className={props.classes.links} to='/AllDrinks'>AllDrinks</NavLink>
+      <NavLink className={props.classes.links} to='/EditDrink'>EditDrink</NavLink>
+      <NavLink className={props.classes.links} to='/ListUsers'>ListUsers</NavLink>
+      <NavLink className={props.classes.links} to='/CreateUser'>CreateUser</NavLink>
+      <NavLink className={props.classes.links} to='/Login'>Login</NavLink>
+      <NavLink className={props.classes.links} to='/TopTen'>TopTen</NavLink>
+      <NavLink className={props.classes.links} to='/UserProfile'>UserProfile</NavLink>
+    </div>
+  )
+}
+
+export default enhancer(NavBar)
