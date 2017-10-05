cfbae9f5659af94fcf89f05aae66b03602b4660d
Travis Surmi <surmi.travis@gmail.com>
2017-10-05 14:01:54 -0700

    one more change to footer

--- client/src/components/structure/Footer.js
+++ client/src/components/structure/Footer.js
@@ -7,17 +7,13 @@ const styles = {
     bottom: 0,
     left: 0,
     right: 0,
-    height: 50,
+    height: 200,
     background: {
-      image: 'url("http://i68.tinypic.com/29c998l.png")'
+      image: 'url("http://i65.tinypic.com/4twy8p.jpg")'
     },
     display: 'flex',
     alignItems: 'center',
     paddingLeft: 20
-  },
-  p: {
-    color: 'white',
-    fontSize: 20
   }
 }
 
@@ -25,7 +21,6 @@ const enhancer = injectSheet(styles)
 
 const Footer = (props) =>
   <div className={props.classes.footer}>
-    <p className={props.classes.p}> A coffee app for coffee people</p>
   </div>
 
 export default enhancer(Footer)
