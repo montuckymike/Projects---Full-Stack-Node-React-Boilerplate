2c14f0e3ce28a178204b1b1540b92ead74a4cc16
Travis Surmi <surmi.travis@gmail.com>
2017-10-06 12:04:03 -0700

    added an avatar

--- client/src/components/pages/admin/AddDrink.js
+++ client/src/components/pages/admin/AddDrink.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Avatar from 'material-ui/Avatar';
 
 const AddDrink = () => {
   return (
--- client/src/components/pages/user/UserProfile.js
+++ client/src/components/pages/user/UserProfile.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import injectSheet from 'react-jss'
+import Avatar from 'material-ui/Avatar'
 
 const styles = {
-  userProfile: {
-    border: 'solid #2980b9',
-    width: '74.5%'
+  row: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  avatar: {
+    margin: 10,
+    width: 100,
+    height: 100
   }
 }
 
 const enhancer = injectSheet(styles)
 
-const UserProfile = (props) => {
+const UserProfile = ({classes}) => {
   return (
-    <div className={props.classes.userProfile}>
-      Profile
+    <div className={classes.userProfile}>
+      <Avatar src='https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png' className={classes.avatar} />
     </div>
   )
 }
