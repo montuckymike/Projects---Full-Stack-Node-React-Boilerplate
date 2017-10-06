57e3935952507e271496963ece139899ba53cf82
Hannah Hendrickson <hannahhendrickson@Hannahs-MBP.PK5001Z>
2017-10-06 12:58:50 -0600

    added add product container and form crap

---
+++ client/src/components/pages/admin/AddDrinkContainer.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import AddDrinkForm from './AddDrinkForm'
+
+class AddDrinkContainer extends Component {
+  render () {
+    return (
+      <AddDrinkForm />
+    )
+  }
+}
+
+export default AddDrinkContainer
