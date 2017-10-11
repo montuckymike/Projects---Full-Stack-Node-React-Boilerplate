66f4168b355fd90e1a6479a67a657a944d92a67f
Travis Surmi <surmi.travis@gmail.com>
2017-10-11 10:21:08 -0700

    Added post route

--- server/routes/drink.js
+++ server/routes/drink.js
@@ -13,5 +13,16 @@ Router.route('/')
       }
     })
   })
-
+  .post((req, res) => {
+    const drink = new Drink()
+    console.log(req.body)
+    drink.loadData(req.body)
+    drink.save((err, drink) => {
+      if (err) {
+        res.json({ message: err, data: null })
+      } else {
+        res.json({ message: 'Added new Drink 😆', data: drink })
+      }
+    })
+  })
 module.exports = Router
