26174ce2f8f6dca1d798262010462467db21c121
Travis Surmi <surmi.travis@gmail.com>
2017-10-13 09:54:19 -0700

    updated profile card

--- client/src/components/pages/user/profile/Profile.js
+++ client/src/components/pages/user/profile/Profile.js
@@ -2,27 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from 'material-ui/styles'
 import {Link} from 'react-router-dom'
-import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card'
+import Card, { CardActions, CardContent } from 'material-ui/Card'
 import Button from 'material-ui/Button'
 
 const propTypes = {
   classes: PropTypes.object.isRequired,
   firstName: PropTypes.string.isRequired,
   lastName: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-  onImageChanged: PropTypes.func.isRequired
+  email: PropTypes.string.isRequired
 }
 
 const styles = {
   card: {
-    maxWidth: 345
+    maxWidth: 345,
+    margin: 20
   },
   media: {
-    height: 200
+    height: 200,
+    width: 325,
+    margin: 10
   },
   randomButton: {
     dense: 'primary',
     textDecoration: 'none'
+  },
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'space-between'
   }
 }
 
@@ -31,17 +37,15 @@ const Profile = props => {
   return (
     <div>
       <Card className={classes.card}>
-        <CardMedia
-          className={classes.media}
-          image={props.onImageChanged}
-          title='Contemplative Reptile'
-        />
+        <div>
+          <img className={classes.media} src='https://burst.shopifycdn.com/photos/making-coffee_925x.jpg' />
+        </div>
         <CardContent>
           <div>First Name: {props.firstName}</div>
           <div>Last Name: {props.lastName}</div>
           <div>Email: {props.email}</div>
         </CardContent>
-        <CardActions>
+        <CardActions className={classes.cardActions}>
           <Button dense color='primary'>
             <Link to='/Randomizer'>Randomizer</Link>
           </Button>
